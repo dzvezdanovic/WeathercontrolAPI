5a99941245e67069b1026d893bcea2d8c86c855c
dzvezdanovic <darko.zvezdanovic@fazi.rs>
2024-09-25 13:34:17 +0200

    Added appsettings.json, added weather controller. Added Iweatherservice interface and implementation for it. Added serilog.

---
+++ WeatherApplication/WeatherAppTests/Unit/WeatherServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApplication.Services.Implementation;
+
+namespace WeatherAppTests.Unit
+{
+    public class WeatherServiceTests
+    {
+        private readonly Mock<HttpMessageHandler> _httpMessageHandlerMock;
+        private readonly WeatherService _weatherService;
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<WeatherService> _logger;
+
+        public WeatherServiceTests()
+        {
+            _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            _logger = Mock.Of<ILogger<WeatherService>>();
+            _weatherService = new WeatherService(_httpClient, _logger);
+        }
+    }
+}
