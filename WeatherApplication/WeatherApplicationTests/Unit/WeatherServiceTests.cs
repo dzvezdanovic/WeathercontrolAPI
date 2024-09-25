f026f1c937b832e05848429f47452d039530e0ef
dzvezdanovic <darko.zvezdanovic@fazi.rs>
2024-09-25 14:59:05 +0200

    Restored csproj files.

---
+++ WeatherApplication/WeatherApplicationTests/Unit/WeatherServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using WeatherApplication.Services.Interfaces;
+
+namespace WeatherApplicationTests.Unit
+{
+    public class WeatherServiceTests
+    {
+        private readonly Mock<HttpMessageHandler> _httpMessageHandlerMock;
+        private readonly IWeatherService _weatherService;
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<IWeatherService> _logger;
+
+        public WeatherServiceTests(IWeatherService weatherService)
+        {
+            _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            _logger = Mock.Of<ILogger<IWeatherService>>();
+            _weatherService = weatherService;
+        }
+    }
+}
---
+++ WeatherCopy/WeatherApplication/Models/WeatherResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApplication.Models
+{
+    public class WeatherResponse
+    {
+        public string City { get; set; }
+        public string Description { get; set; }
+        public double Temperature { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
