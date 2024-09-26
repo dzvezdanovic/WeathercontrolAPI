4614bab6e15622913968093197a241f2549c0f96
dzvezdanovic <darko.zvezdanovic@fazi.rs>
2024-09-26 12:57:01 +0200

    Added await for service to resolve. Deleted redudant get methods.

--- WeatherApplication/WeatherApplication/Controllers/WeatherController.cs
+++ WeatherApplication/WeatherApplication/Controllers/WeatherController.cs
@@ -4,7 +4,7 @@ using WeatherApplication.Services.Interfaces;
 namespace WeatherApplication.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/{city}")]
     public class WeatherController : Controller
     {
         private readonly IWeatherService _weatherService;
@@ -16,52 +16,8 @@ namespace WeatherApplication.Controllers
             _logger = logger;
         }
 
-        [HttpGet("{city}")]
-        public async Task<IActionResult> GetWeather(string city)
-        {
-            _logger.LogInformation($"Received request for weather data in {city}");
-
-            if (string.IsNullOrWhiteSpace(city))
-            {
-                return BadRequest("City name cannot be empty.");
-            }
-
-            try
-            {
-                var weatherData = await _weatherService.GetWeatherForCityAsync(city);
-                return Ok(weatherData);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error fetching weather data: {ex.Message}");
-                return BadRequest($"Error fetching weather data: {ex.Message}");
-            }
-        }
-
-        [HttpGet("{city}/{date}")]
-        public async Task<IActionResult> GetWeather(string city, DateTime date)
-        {
-            _logger.LogInformation($"Received request for weather data in {city}");
-
-            if (string.IsNullOrWhiteSpace(city))
-            {
-                return BadRequest("City name cannot be empty.");
-            }
-
-            try
-            {
-                var weatherData = await _weatherService.GetWeatherForCityAndTimeAsync(city, date);
-                return Ok(weatherData);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error fetching weather data: {ex.Message}");
-                return BadRequest($"Error fetching weather data: {ex.Message}");
-            }
-        }
-
         [HttpGet]
-        public async Task<IActionResult> GetWeather([FromQuery] string city, [FromQuery] DateTime? date = null)
+        public async Task<IActionResult> GetWeather([FromRoute] string city, [FromQuery] DateTime? date = null)
         {
             _logger.LogInformation($"Received request for weather data in {city}");
 
@@ -72,12 +28,12 @@ namespace WeatherApplication.Controllers
 
             if (date.HasValue)
             {
-                var weatherForSpecificDate = _weatherService.GetWeatherForCityAndTimeAsync(city, date.Value);
+                var weatherForSpecificDate = await _weatherService.GetWeatherForCityAndTimeAsync(city, date.Value);
                 return Ok(weatherForSpecificDate);
             }
             else
             {
-                var currenetWeather = _weatherService.GetWeatherForCityAsync(city);
+                var currenetWeather = await _weatherService.GetWeatherForCityAsync(city);
                 return Ok(currenetWeather);
             }
         }
